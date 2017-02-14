ee0a547efae9a83b4c1e4ca2b2d9dbcd612726a1
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-02-14 07:30:19 +0000

    [AsyncFiber] Implement run(until date: Date)

--- Sources/AsyncFiber/AsyncFiber.swift
+++ Sources/AsyncFiber/AsyncFiber.swift
@@ -12,26 +12,27 @@ import Async
 import Fiber
 import Foundation
 
-public struct Loop: AsyncLoop {
-    public func run() {
-        FiberLoop.current.run()
-    }
-}
-
 public struct AsyncFiber: Async {
-    public init() {}
+    public struct Loop: AsyncLoop {
+        public func run() {
+            FiberLoop.current.run()
+        }
 
-    public var loop: AsyncLoop = Loop()
-
-    public var task: (@escaping AsyncTask) -> Void = fiber
-
-    public var awaiter: IOAwaiter? = FiberAwaiter()
-}
-
-public struct FiberAwaiter: IOAwaiter {
-    public init() {}
-
-    public func wait(for descriptor: Descriptor, event: IOEvent) throws {
-        try FiberLoop.current.wait(for: descriptor, event: event, deadline: Date.distantFuture)
+        public func run(until date: Date) {
+            FiberLoop.current.run(until: date)
+        }
     }
+
+    public struct Awaiter: IOAwaiter {
+        public init() {}
+
+        public func wait(for descriptor: Descriptor, event: IOEvent) throws {
+            try FiberLoop.current.wait(for: descriptor, event: event, deadline: Date.distantFuture)
+        }
+    }
+
+    public init() {}
+    public var loop: AsyncLoop = Loop()
+    public var task: (@escaping AsyncTask) -> Void = fiber
+    public var awaiter: IOAwaiter? = Awaiter()
 }
