bc9fcd7c45078e4644f8f8a127ce235a866d49f3
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-05-23 18:31:22 +0000

    Inline globals

--- Sources/Fiber/Globals.swift
+++ Sources/Fiber/Globals.swift
@@ -13,23 +13,28 @@ import Async
 import struct Foundation.Date
 import struct Dispatch.DispatchQoS
 
+@inline(__always)
 public func fiber(_ task: @escaping AsyncTask) {
     FiberLoop.current.scheduler.async(task)
 }
 
+@inline(__always)
 public func yield() {
     FiberLoop.current.scheduler.yield()
 }
 
+@inline(__always)
 public func sleep(until deadline: Date) {
     FiberLoop.current.wait(for: deadline)
 }
 
+@inline(__always)
 public func now() -> Date {
     return FiberLoop.current.now
 }
 
 /// Spawn DispatchQueue.global().async task and yield until it's done
+@inline(__always)
 public func dispatch<T>(
     qos: DispatchQoS.QoSClass = .background,
     deadline: Date = Date.distantFuture,
