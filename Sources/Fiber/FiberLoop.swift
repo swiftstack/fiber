0ffb4dc124782e07ba6a1805e29fbafe14efb57b
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-05-23 17:06:00 +0300

    Add currentFiber property to FiberLoop

--- Sources/Fiber/FiberLoop.swift
+++ Sources/Fiber/FiberLoop.swift
@@ -48,6 +48,12 @@ public class FiberLoop {
 
     var scheduler: FiberScheduler
 
+    var currentFiber: UnsafeMutablePointer<Fiber> {
+        @inline (__always) get {
+            return scheduler.running
+        }
+    }
+
     public private(set) static var main = FiberLoop()
     private static var _current = ThreadSpecific<FiberLoop>()
     public class var current: FiberLoop {
@@ -156,7 +162,7 @@ public class FiberLoop {
     }
 
     public func wait(for deadline: Deadline) {
-        let watcher = Watcher(fiber: scheduler.running, deadline: deadline)
+        let watcher = Watcher(fiber: currentFiber, deadline: deadline)
         add(watcher)
         scheduler.sleep()
         remove(watcher)
@@ -172,7 +178,7 @@ public class FiberLoop {
     }
 
     public func wait(for socket: Descriptor, event: IOEvent, deadline: Deadline) throws {
-        let watcher = Watcher(fiber: scheduler.running, deadline: deadline)
+        let watcher = Watcher(fiber: currentFiber, deadline: deadline)
         try add(watcher, for: socket, event: event)
         scheduler.sleep()
         remove(watcher, for: socket, event: event)
