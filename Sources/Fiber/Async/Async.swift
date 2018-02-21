981bec83f182a01d18dde400cf9e0b444631ff14
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-02-21 11:49:08 +0000

    Use new Async APIs

---
+++ Sources/Fiber/Async/Async.swift
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2017 Tris Foundation and the project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License
+ *
+ * See LICENSE.txt in the project root for license information
+ * See CONTRIBUTORS.txt for the list of the project authors
+ */
+
+import Async
+
+extension Fiber: Asynchronous {
+    public static var async: Async {
+        return AsyncFiber()
+    }
+}
