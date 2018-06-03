3fba27f19cde4c143e8bee089d5241979d29085d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-06-03 07:17:26 +0000

    [Tests] Test ARC memory leak

--- Tests/FiberTests/FiberTests.swift
+++ Tests/FiberTests/FiberTests.swift
@@ -12,8 +12,6 @@
 import Test
 @testable import Fiber
 
-import struct Foundation.Date
-
 class FiberTests: TestCase {
     func testFiber() {
         var a = 0
@@ -33,5 +31,35 @@ class FiberTests: TestCase {
         }
 
         FiberLoop.current.run()
+    }
+
+    func testMemoryLeak() {
+        var released = false
+
+        class Test {
+            var task: () -> Void
+            init(_ task: @escaping () -> Void) {
+                self.task = task
+            }
+            @inline(never)
+            func nop() {}
+            deinit { task() }
+        }
+
+        scope {
+            let test = Test {
+                released = true
+            }
+            fiber {
+                test.nop()
+                yield()
+                test.nop()
+                // TODO: document to use `weak` pointer or release manually
+                Unmanaged.passUnretained(test).release()
+            }
+        }
+
+        FiberLoop.current.run()
+        assertTrue(released)
     }
 }
